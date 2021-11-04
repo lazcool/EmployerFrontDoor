4e2acfb92950269f5be1d80424a9582aee7ab547
Phil Davies <github@phildavies.co.uk>
2021-11-04 14:07:52 +0000

    initial site + contentful package

---
+++ EmployerFrontDoor.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployerFrontDoor.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
